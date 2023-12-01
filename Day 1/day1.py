7149cf9690b7844d0e1655518d4a1e791ca8614d
GoldenDrummer730 <golden.drummer730@gmail.com>
2023-12-01 15:22:28 -0500

    Added Day 1 solution

---
+++ Day 1/day1.py
@@ -0,0 +1,34 @@
+import re
+
+sum = 0
+
+conv = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+}
+
+with open(r'AdventOfCode2023\Day 1\input.txt') as input:
+    for line in input:
+        pattern = re.compile(
+            '(?=(one|two|three|four|five|six|seven|eight|nine|\d))'
+        )
+        matches = pattern.findall(line)
+
+        if (conv.get(matches[0])):
+            matches[0] = conv.get(matches[0])
+
+        if (conv.get(matches[-1])):
+            matches[-1] = conv.get(matches[-1])
+
+        curr = int(matches[0] + matches[-1])
+
+        sum += curr
+
+print(sum)
