c226cd3341626757cb4b7ed5973a859acce5a053
GoldenDrummer730 <golden.drummer730@gmail.com>
2023-12-02 02:10:26 -0500

    Finished Day 2

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -5,39 +5,71 @@ red_total = 12
 green_total = 13
 blue_total = 14
 
-totals = {
-    'red': 12,
-    'green': 13,
-    'blue': 14
-}
+result = 0
 
 def is_possible(sets: list[str]):
-    red_shown = 0
-    green_shown = 0
-    blue_shown = 0
     for game in sets:
         for curr_game in game.split(','):
-            curr_game = curr_game.strip()
-            if (re.findall('red', curr_game)):
-                red_shown += int(curr_game[0, curr_game.find(' ')])
-            elif (re.findall('green', curr_game)):
-                green_shown += int(curr_game[0, curr_game.find(' ')])
-            elif (re.findall('blue', curr_game)):
-                blue_shown += int(curr_game[0, curr_game.find(' ')])
+            red_shown = 0
+            green_shown = 0
+            blue_shown = 0
             
-        print(f'Red: {red_shown}\tGreen: {green_shown}\tBlue: {blue_shown}\n')
-        if ((red_shown > red_total) or 
-            (green_shown > green_total) or
-            (blue_shown > blue_total)):
-                return False
+            curr_game = curr_game.strip().split(' ')
+
+            if (re.findall('red', curr_game[1])):
+                red_shown += int(curr_game[0])
+            
+            elif (re.findall('green', curr_game[1])):
+                green_shown += int(curr_game[0])
+            
+            elif (re.findall('blue', curr_game[1])):
+                blue_shown += int(curr_game[0])
+            
+        # print(f'Red: {red_shown}\tGreen: {green_shown}\tBlue: {blue_shown}\n')
+            if ((red_shown > red_total) or 
+                (green_shown > green_total) or
+                (blue_shown > blue_total)):
+                    return False
     return True
 
-with open(r'Day 2\sample.txt') as input:
+
+def least_possible(sets: list[str]):
+    power = 0
+    min_red = 0
+    min_green = 0
+    min_blue = 0
+    for game in sets:
+        for curr_game in game.split(','):
+            curr_game = curr_game.strip().split(' ')
+
+            if (re.findall('red', curr_game[1])):
+                min_red = max(min_red, int(curr_game[0]))
+            
+            elif (re.findall('green', curr_game[1])):
+                min_green = max(min_green, int(curr_game[0]))
+            
+            elif (re.findall('blue', curr_game[1])):
+                min_blue = max(min_blue, int(curr_game[0]))
+        
+    #print(f'Red: {min_red}\tGreen: {min_green}\tBlue: {min_blue}\n')
+
+    return min_red * min_green * min_blue
+
+
+with open(r'Day 2\input.txt') as input:
     for line in input:
         # First grab the id of the game
         groups = re.split(pattern=':', string=line)
-        id = groups[0][-1]
+        id = int(groups[0].split(' ')[-1])
         
         sets = groups[1].replace('\n', '').split(';')
-        print(is_possible(sets))
-        print('===============')
+
+        # Part 1
+        # if(is_possible(sets)):
+        #     print(id)
+        #     result += id
+    
+        # Part 2
+        result += least_possible(sets)
+
+print(result)
